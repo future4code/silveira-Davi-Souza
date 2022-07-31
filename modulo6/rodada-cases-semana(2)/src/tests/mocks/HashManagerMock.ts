e22938cec0206d910f2ead83dde929c19f48992c
Davi Souza <davi.231souza@gmail.com>
2022-07-31 13:57:14 -0300

    atualizando

--- modulo6/rodada-cases-semana(2)/src/tests/mocks/HashManagerMock.ts
+++
@@ -1,9 +0,0 @@
-export class HashManagerMock {
-    hash = async (plainText: string): Promise<string> => {
-        return "hash";
-    };
-
-    compare = async (plainText: string, cypherText: string): Promise<boolean> => {
-        return plainText === cypherText;
-    };
-};
--- modulo6/rodada-cases-semana(2)/src/tests/mocks/UserMock.ts
+++
@@ -1,8 +0,0 @@
-import User from "../../model/User";
-
-export const userMock = new User (
-    "user_id_1",
-    "user_name",
-    "user_email@gmail.com",
-    "12345678"
-);
